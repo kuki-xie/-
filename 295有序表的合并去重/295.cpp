1cce686b5f444971d5ca5c701bf506c282649528
kuki-xie <xieronghaii@outlook.com>
2020-11-20 23:21:09 +0800

    debug没问题，输出错

--- 295有序表的合并去重/295.cpp
+++ 295有序表的合并去重/295.cpp
@@ -6,6 +6,7 @@
 //
 
 #include <iostream>
+#include <set>
 using namespace std;
 int main(int argc, const char * argv[]) {
     int a,b;//分别表示第一个线性表L1和第二个线性表L2的长度
@@ -25,16 +26,19 @@ int main(int argc, const char * argv[]) {
     for (i = 0; i < b; i++) {
         cout << L2[i];
     }*/
-    int count = 0;//用来记输出数组fin的长度
-    int fin[200];//合并去重后输出用
-    int len = (a < b) ? a : b;//三元运算符,取L1和L2之间最短的长度
-    for (i = 0; i < len; i++) {//先比较各自的前len项
-        if(L1[i] < L2[i] || L1[i] == L2[i]){//如果相应位置L1元素比L2小或相等，则存入L1的元素
-            fin[count++]=L1[i];
-        }
-        else if(L1[i] > L2[i])//相应位置L1元素比L2元素大，则存入L2的元素
-            fin[count++]=L2[i];
+    //int count = 0;//用来记输出数组fin的长度
+    set<int> s;//set容器，key不可以重复，合并去重后输出用
+    set<int>::iterator iter;
+    for (i = 0; i < a; i++) {//循环插入L1中元素
+        s.insert(L1[i]);
     }
+    for (i = 0; i < b; i++) {//循环插入L2中的元素
+        s.insert(L2[i]);
+    }
+    for (iter = s.begin(); iter != s.end(); iter++) {//迭代器输出set中元素
+        cout << *iter << ' ';
+    }
+    /*int len = (a < b) ? a : b;//三元运算符,取L1和L2之间最短的长度
     if(a < b){//L1数组比L2数组短
         for (i = len; i < b; i++) {
             fin[count++]=L2[i];
@@ -47,6 +51,6 @@ int main(int argc, const char * argv[]) {
     }
     for (i = 0; i < count; i++) {//输出合并去重后的结果。
         cout << fin[i] << ' ';
-    }
+    }*/
     return 0;
 }
