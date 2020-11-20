a0a55cdac78d308c4b7ff65999395761ac276bf3
kuki-xie <xieronghaii@outlook.com>
2020-11-20 21:31:41 +0800

    未开始

--- 297最小周期串/297.cpp
+++ 297最小周期串/297.cpp
@@ -9,22 +9,5 @@
 using namespace std;
 int main(int argc, const char * argv[]) {
     int a,b;//分别表示第一个线性表L1和第二个线性表L2的长度
-    cin >> a >> b;//第一行输入两个整数数a,b,元素值小于100个
-    int i;
-    int L1[100];
-    int L2[100];
-    for (i = 0; i < a; i++) {//第二行依次输入L1中单调递增的a个正整数
-        cin >> L1[i];
-    }
-    for (i = 0; i < b; i++) {//第三行依次输入L2中单调递增的b个正整数
-        cin >> L2[i];
-    }
-    /*for (i = 0; i < a; i++) {//检查输入是否正确
-        cout << L1[i];
-    }
-    for (i = 0; i < b; i++) {
-        cout << L2[i];
-    }*/
-    int fin[200];//输出用
     return 0;
 }
