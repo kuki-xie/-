d0c792529da7c79140517c32c401fc78a5895f5c
kuki-xie <xieronghaii@outlook.com>
2020-11-22 22:29:55 +0800

    正确

--- 297最小周期串/297.cpp
+++ 297最小周期串/297.cpp
@@ -6,8 +6,30 @@
 //
 
 #include <iostream>
+#include <string>
 using namespace std;
 int main(int argc, const char * argv[]) {
-    int a,b;//分别表示第一个线性表L1和第二个线性表L2的长度
-    return 0;
+    
+    
+    char a[81];
+    int T;
+    while(scanf("%d",&T)!=EOF)
+    {
+        scanf("%s",a);//输入一个长度不超过80的串
+        int len = (int)strlen(a);//len——字符串长度
+        for(int i = 1 ; i <= len ; i++)//循环读取字符串,寻找最小周期
+        if(!(len % i)){//比如len是9，寻找质数，如果是4，就不可能形成周期
+            for(int j = 0;j < len;j++){//在假设周期i的条件下
+                if(a[j]!=a[j % i])//判断后面每一段字符是否都等于第一段字符
+                    break;
+                if(j == len-1)//判断是否到最后一位，到了就可以输出结果
+                {
+                    printf("%d\n",i);
+                    i = len+1;//中断外层循环，因为结果已经找到了，再循环没有意义了。
+                }
+            }
+        }
+        return 0;
+        
+    }
 }
