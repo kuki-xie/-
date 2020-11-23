4976da141efedb36fb6f6c61c618ff7ab92ff777
kuki-xie <xieronghaii@outlook.com>
2020-11-23 20:35:40 +0800

    未解决

---
+++ 287求矩阵中的马鞍点/main.cpp
@@ -0,0 +1,72 @@
+//
+//  main.cpp
+//  287求矩阵中的马鞍点
+//
+//  Created by 谢容海 on 2020/11/23.
+//
+
+#include <iostream>
+using namespace std;
+typedef struct node{
+    int row;//定义行
+    int cow;//定义列
+    int data;//定义元素所含值
+}node;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int n,m;//为矩阵维数
+    int arr[5][5];//储存输入的矩阵中的元素值
+    //输入
+    cin >> n >> m;//输入矩阵的维数，m为行数，n为列数
+    int i,j;
+    for(i = 0;i < m; i++){//通过二维数组输入并存储二维矩阵的元素值
+        for (j = 0; j < n; j++) {
+            cin >> arr[i][j];//i代表行，j代表列
+        }
+    }
+    /*for(i = 0;i < m; i++){//检查输入是否正确
+        for (j = 0; j < n; j++) {
+            cout << arr[i][j] << ' ';
+        }
+    }*/
+    //遍历寻找马鞍点
+    int count = 0;//计数器，数有几个马鞍点
+    node ans[50];//用来存储所找到马鞍点的信息
+    int a = 0;//a代表最小值所在列
+    int b = 0;//b代表最大值所在行
+    int k = 0;
+    for (i = 0; i < m; i++) {
+        int min = arr[i][0];//初始定义第i行中的最小值
+                for (j = 0; j < n; j++) {//寻找i行中的最小值
+            if(min > arr[i][j]){//如果第i行有比初始定义的min更小的，就用这个去替换
+                min = arr[i][j];
+                a = j;//a代表当前行中最小的列数
+            }
+        }
+
+        int max= arr[0][a];//初始定义上面所找的最小值所在列的第a列中的最大值
+        for(k = 0; k < m; k++){//寻找第j列中的最大值
+            if(max < arr[k][a]){//如果第j列有比初始定义的max更大的，则将其赋值为最大值
+                max = arr[k][a];
+                b = k;//b为此列的最大值所在行数
+            }
+        }
+        if (max == min) {
+            ans[count].row = m;//存储所找的行数
+            ans[count].cow = n;//存储所找的列数
+            ans[count].data = max;//存储所找的值
+            count++;
+        }
+    }
+    //处理输出
+    if (count == 0) {
+        cout << "NO" << endl;//没有马鞍点输出NO
+    }
+    else{
+        for (i = 0; i < count; i++) {
+            cout << ans[i].row + 1<< ' ' << ans[i].cow + 1<< ans[i].data << endl;
+        }
+    }
+    //std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ 292huffman编码/main.cpp
@@ -0,0 +1,124 @@
+//
+//  main.cpp
+//  292huffman编码
+//
+//  Created by 谢容海 on 2020/11/23.
+//
+
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node{
+    int data;//表示权值
+    struct node *next;//下一个结点的地址
+}Linklist;
+//创建链表
+Linklist* create(int array[],int n){//创建哈夫曼树
+    int i;
+    Linklist *head,*p,*q;//分别表示头结点和左孩子，右孩子
+    head=(Linklist *)malloc(sizeof(Linklist));//分配内存
+    head->next=p=q=NULL;//初始化
+    for(i=0;i<n;i++){
+        q=(Linklist *)malloc(sizeof(Linklist));//分配内存
+        q->data=array[i];//将链表中的每一个权值都赋给q
+        q->next=NULL;
+        if(head->next==NULL){
+            head->next=q;
+            p=q;
+        }else{
+            p->next=q;
+            p=q;
+        }
+    }
+    return head;
+}
+//将数字有序插入
+Linklist * insert(Linklist *head,int num){
+    Linklist *p,*q;
+    q=(Linklist *)malloc(sizeof(Linklist));//分配内存
+    q->data=num;
+    q->next=NULL;
+    p=head;
+    if(head->next==NULL){//最后一次为空还需要插入值
+        head->next=q;
+    }else{
+        while(p->next!=NULL&&num>p->next->data){
+            p=p->next;
+        }
+        q->next=p->next;
+        p->next=q;
+    }
+    return head;
+}
+//删除头元素
+Linklist *delHead(Linklist *head){
+    Linklist *p;
+    p=head->next;
+    head->next=p->next;
+    free(p);
+    return head;
+}
+int main(void){
+    int n,i,j,temp;
+    int array[41];
+    int sum,sum1,sum2,count;
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&array[i]);
+    }
+    //给数组排序
+    for(i=1;i<n;i++){
+        for(j=i;j>0;j--){
+            if(array[j]<array[j-1]){
+                temp=array[j];
+                array[j]=array[j-1];
+                array[j-1]=temp;
+            }
+        }
+    }
+    //创建链表
+    Linklist *head=create(array,n);
+    Linklist *p;
+    //计算频度
+    count=0;
+    while(head->next->next!=NULL){
+        sum1=head->next->data;
+        head=delHead(head);
+        sum2=head->next->data;
+        head=delHead(head);
+        sum=sum1+sum2;
+        count+=sum;
+        head=insert(head,sum);
+    }
+    printf("%d",count);
+    return 0;
+}
+
+/*
+#include <iostream>
+using namespace std;
+struct BiTreeNode{
+    int data;//表示权值
+    BiTreeNode* lchild;//左孩子
+    BiTreeNode* rchild;//右孩子
+}bnode;
+
+BiTreeNode* createHuffman(int arr[n],int n){
+    BiTreeNode ** b,
+}
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int n;//n的值，表示电文由n个字符组成
+    int i;
+    cin >> n;
+    int arr[31];//4<n<30
+    for (i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    /**for (i = 0; i < n; i++) {//检查输入是否成功
+        cout << arr[i] << ' ';
+    }*/
+    
+    //std::cout << "Hello, World!\n";
+    /*return 0;
+}
+*/
