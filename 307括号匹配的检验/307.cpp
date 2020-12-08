06e98ef816512ffd5de103c703770f1a8aee33af
kuki-xie <xieronghaii@outlook.com>
2020-12-08 20:49:57 +0800

    40

---
+++ 307括号匹配的检验/307.cpp
@@ -0,0 +1,56 @@
+//
+//  main.cpp
+//  307括号匹配的检验
+//
+//  Created by 谢容海 on 2020/12/8.
+//
+
+#include <iostream>
+#include <stack>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    stack<char> sc;// 定义一个栈的对象
+    char str[50];// 创建一个字符数组
+    char* arr = str;// 为指针分配内存
+    scanf("%c",arr);// 输入表达式
+    int flag = 1;
+    while(*arr != '\0' && flag == 1){
+        if (*arr == '(' || *arr == '[' || *arr == '{') {//把括号的左部分入栈
+            sc.push(*arr);
+        }
+        else if (*arr == ')'){// 遇到')'
+            if (sc.top() == '(') {// 看栈顶元素是不是它的左半部分
+                sc.pop();// 是，则弹出
+            }
+            else{//不是，则不匹配
+                cout << "wrong" << endl;
+                flag = 0;
+            }
+        }
+        else if (*arr == ']') {// 遇到']'
+            if (sc.top() == '[') {// 看栈顶元素是不是它的左半部分
+                sc.pop();// 是，则弹出
+            }
+            else{
+                cout << "wrong" << endl;
+                flag = 0;
+            }
+        }
+        else if (*arr == '}') {// 遇到'}'
+            if (sc.top() == '{') {// 看栈顶元素是不是它的左半部分
+                sc.pop();// 是，则弹出
+            }
+            else{
+                cout << "wrong" << endl;
+                flag = 0;
+            }
+        }
+        arr++;
+    }
+    if (sc.size() == 0) {
+        cout << "right" << endl;
+    }
+    //std::cout << "Hello, World!\n";
+    return 0;
+}
