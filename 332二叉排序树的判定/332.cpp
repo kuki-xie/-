bf9c42ad932a36f50fcaba251bf9db8cbe80e15c
kuki-xie <xieronghaii@outlook.com>
2020-12-16 13:30:43 +0800

    正确

--- 332二叉排序树的判定/332.cpp
+++ 332二叉排序树的判定/332.cpp
@@ -11,13 +11,13 @@ struct node{
     int val;// 节点的关键字
     node* lchild;// 左孩子
     node* rchild;// 右孩子
-    node(int x): val(x),lchild(NULL),rchild(NULL){}
+    node(int x): val(x),lchild(NULL),rchild(NULL){}// 把x赋给val，left和right赋值NULL
 };
-bool isBiTree(int& pre,node *root){
-    if (root == NULL) {
+bool isBiTree(int& pre,node *root){// 判断是否为二叉排序树
+    if (root == NULL) {// 如果为空
         return true;
     }
-    if (!isBiTree(pre, root -> lchild)) {
+    if (!isBiTree(pre, root -> lchild)) {// 如果左子树不是二叉排序树
         return false;
     }
     if (pre >= root -> val) {
@@ -25,21 +25,22 @@ bool isBiTree(int& pre,node *root){
     }
     else
         pre = root -> val;
-    if (!isBiTree(pre, root -> rchild)) {
+    if (!isBiTree(pre, root -> rchild)) {// 如果右子树不是二叉排序树
         return false;
     }
     return true;;
 }
 int main(int argc, const char * argv[]) {
-    int n,t,v,l,r;
+    int n,t; // n个结点，第t个结点为根结点
+    int v,l,r;// 定义结点的值为v,第l个为左孩子，第r个为右孩子
     cin >> n >> t;// n个结点，第t个结点是根结点
-    node* T = (node*)malloc(sizeof(node));
+    node* T = (node*)malloc(sizeof(node)*(n + 1));// 
     node* R = &T[t];
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
         cin >> v >> l >> r;
         T[i].val = v;
         T[i].lchild = l ? &T[l] : 0;
-        T[i].rchild = 1 ? &T[r] : 0;
+        T[i].rchild = r ? &T[r] : 0;
     }
     int pre = -1;
     cout << (isBiTree(pre, R) ? "true" : "false") << endl;
